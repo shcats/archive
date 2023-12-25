62caf8e50efbf19337ed7490a02c2972af1734af
Alexander <shcats@gmail.com>
2023-12-25 23:22:41 +0300

    add pass

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ from pyunpack import Archive
 
 
 # Archive(filename="MicroTik.rar", password="123").extractall(".")
+
+pass_lab = ""
+
+with open("pass.txt") as f:
+    pass_lab = f.readlines()
+
+
+print(pass_lab)
